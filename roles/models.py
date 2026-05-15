30a063c6ac1f2670cf62a14d0d4ffc9a4abf6461
raulodev <raulodev@gmail.com>
2026-05-15 06:01:43 -0400

    feat: Alter field name on role

--- roles/models.py
+++ roles/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class DefaultSystemRole(models.TextChoices):
@@ -13,7 +14,7 @@ class DefaultSystemRole(models.TextChoices):
 class Role(models.Model):
     group = models.OneToOneField(Group, on_delete=models.CASCADE, related_name="role")
     name = models.CharField(
-        max_length=16, unique=True, choices=DefaultSystemRole.choices, db_index=True
+        verbose_name=_("name"), max_length=50, unique=True, db_index=True
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
