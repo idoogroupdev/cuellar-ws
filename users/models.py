a46e82bb9c7e6ac46cb949b1d9ebcbbdf150fb7c
raulodev <raulodev@gmail.com>
2026-05-16 08:41:10 -0400

    feat: add roles field to User model and create migration

--- users/models.py
+++ users/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from roles.models import Role
 
 
 class User(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
+    roles = models.ManyToManyField(Role)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
