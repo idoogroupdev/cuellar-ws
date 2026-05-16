a4da546d76b932eb71b001580c7791916e096749
raulodev <raulodev@gmail.com>
2026-05-16 18:40:53 -0400

    feat: add client_query fixture for GraphQL testing with client context

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from graphene_django.utils.testing import graphql_query
 
 from roles.management.commands import setup_roles as setup_roles_command
 
@@ -6,3 +7,11 @@ from roles.management.commands import setup_roles as setup_roles_command
 @pytest.fixture
 def setup_system_roles():
     setup_roles_command.Command().handle()
+
+
+@pytest.fixture
+def client_query(client, setup_system_roles):
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
