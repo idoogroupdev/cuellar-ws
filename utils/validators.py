76a9a58f10925275e15b7d55ecd1c53945bce713
raulodev <raulodev@gmail.com>
2026-05-18 09:11:51 -0400

    feat: add password validator

--- utils/validators.py
+++ utils/validators.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.password_validation import (
+    validate_password as _validate_password,
+)
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email as _validate_email
 
@@ -7,3 +10,10 @@ def validate_email(email: str):
         _validate_email(email)
     except ValidationError as exc:
         raise ValidationError({"email": exc.message}) from exc
+
+
+def validate_password(password: str):
+    try:
+        _validate_password(password)
+    except ValidationError as exc:
+        raise ValidationError({"password": exc.messages[0]}) from exc
