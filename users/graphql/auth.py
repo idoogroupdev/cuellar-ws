9b45c75f2aadf6af44f32fa42dc4d3944c22b757
raulodev <raulodev@gmail.com>
2026-05-18 11:01:53 -0400

    feat: add RequestAuthCode mutation

--- users/graphql/auth.py
+++ users/graphql/auth.py
@@ -1,11 +1,18 @@
 import graphene
 import graphql_jwt
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 
 from roles.models import DefaultSystemRole
 from users.graphql.user import UserNode
+from users.services.auth_service import AuthService
 from users.services.user_service import UserService
 from utils.exceptions import UserNotVerified, ValidationGraphQLError
+
+
+class AuthCodeEnum(graphene.Enum):
+    REGISTRATION = "REGISTRATION"
+    PASSWORD_RECOVERY = "PASSWORD_RECOVERY"  # nosec
 
 
 class RegisterUserInput(graphene.InputObjectType):
@@ -42,12 +49,32 @@ class Login(graphql_jwt.ObtainJSONWebToken):
         return cls(user=info.context.user)
 
 
+class RequestAuthCodeInput(graphene.InputObjectType):
+    email = graphene.String(required=True)
+    auth_code = AuthCodeEnum(required=True)
+
+
+class RequestAuthCode(graphene.Mutation):
+    message = graphene.String()
+
+    class Arguments:
+        input = RequestAuthCodeInput(required=True)
+
+    def mutate(self, info, input: RequestAuthCodeInput):
+        AuthService.send_auth_code(
+            email=input.email.lower(),
+            auth_code=input.auth_code,
+        )
+        return RequestAuthCode(message=_("Code sent"))
+
+
 class Mutation(graphene.ObjectType):
     login = Login.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     revoke_token = graphql_jwt.Revoke.Field()
     register_client = RegisterClient.Field()
+    request_auth_code = RequestAuthCode.Field()
 
 
 schema = graphene.Schema(mutation=Mutation)
