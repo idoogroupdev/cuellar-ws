1fca51fa6c3280b0529675d243dcc823ab6acb89
raulodev <raulodev@gmail.com>
2026-05-21 13:09:48 -0400

    feat: implement send_password_recovery_code

--- services/mail_service.py
+++ services/mail_service.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from users.tasks import send_email_task
 from utils.functions.get_email_content import get_email_content
 
@@ -6,11 +8,23 @@ class MailService:
     @staticmethod
     def send_registration_code(email: str, code: str):
         html, text = get_email_content(
-            template_name="emails/account_verification_email", context={"code": code}
+            template_name="emails/account_verification", context={"code": code}
         )
         send_email_task.delay(
             emails=[email],
-            subject="Verication code",
+            subject=_("Verification code"),
+            message=text,
+            html_message=html,
+        )
+
+    @staticmethod
+    def send_password_recovery_code(email: str, code: str):
+        html, text = get_email_content(
+            template_name="emails/password_recovery", context={"code": code}
+        )
+        send_email_task.delay(
+            emails=[email],
+            subject=_("Password recovery code"),
             message=text,
             html_message=html,
         )
