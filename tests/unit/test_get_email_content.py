810e5def8abb689a6aee81dca0d065f3f1b3cde2
raulodev <raulodev@gmail.com>
2026-05-21 14:11:32 -0400

    fix(tests) : update template_name

--- tests/unit/test_get_email_content.py
+++ tests/unit/test_get_email_content.py
@@ -6,7 +6,7 @@ def test_get_email_content():
     code = "111111"
 
     html_content, text_content = get_email_content(
-        "emails/account_verification_email", {"code": code}
+        "emails/account_verification", {"code": code}
     )
 
     assert code in text_content
