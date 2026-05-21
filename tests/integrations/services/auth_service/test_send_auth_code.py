d9b91b80c90769d65d94ac5926f7022398ae9d20
raulodev <raulodev@gmail.com>
2026-05-21 13:16:49 -0400

    feat: implement send password recovery code

--- tests/integrations/services/auth_service/test_send_auth_code.py
+++ tests/integrations/services/auth_service/test_send_auth_code.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from tests.autofixture import AutoFixture
 from users.models import User
 from users.services.auth_service import AuthCodeEnum, AuthService
+from utils.exceptions import TooManyAttempts
 
 
 @pytest.mark.django_db
@@ -31,4 +32,31 @@ def test_send_auth_code_user_verified():
     with pytest.raises(ValueError, match="User is already verified"):
         AuthService.send_auth_code(
             email=user.email, auth_code=AuthCodeEnum.REGISTRATION
+        )
+
+
+@pytest.mark.django_db
+def test_send_auth_code():
+    user = AutoFixture(User).create()
+
+    AuthService.send_auth_code(email=user.email, auth_code=AuthCodeEnum.REGISTRATION)
+
+    assert user.verification.code
+    assert user.verification.expires_at
+
+    with pytest.raises(TooManyAttempts):
+        AuthService.send_auth_code(
+            email=user.email, auth_code=AuthCodeEnum.REGISTRATION
+        )
+
+    AuthService.send_auth_code(
+        email=user.email, auth_code=AuthCodeEnum.PASSWORD_RECOVERY
+    )
+
+    assert user.recover_password.code
+    assert user.recover_password.expires_at
+
+    with pytest.raises(TooManyAttempts):
+        AuthService.send_auth_code(
+            email=user.email, auth_code=AuthCodeEnum.PASSWORD_RECOVERY
         )
--- users/services/auth_service.py
+++ users/services/auth_service.py
@@ -9,7 +9,7 @@ from graphql_jwt.utils import jwt_decode
 
 from services.mail_service import MailService
 from services.rate_limiter import RateLimiter
-from users.models import AccountVerification, User
+from users.models import AccountVerification, RecoverPassword, User
 from utils.exceptions import TooManyAttempts
 from utils.validators import validate_email
 
@@ -57,10 +57,10 @@ class AuthService:
         if not user.is_active:
             raise ValueError(_("User is not active"))
 
-        if user.is_verified:
-            raise ValueError(_("User is already verified"))
-
         if auth_code.value == AuthCodeEnum.REGISTRATION.value:
+            if user.is_verified:
+                raise ValueError(_("User is already verified"))
+
             result = RateLimiter.hit(
                 scope="req_auth_code_regis",
                 key=email,
@@ -76,8 +76,19 @@ class AuthService:
             MailService.send_registration_code(email, verification.code)
 
         if auth_code.value == AuthCodeEnum.PASSWORD_RECOVERY.value:
-            # TODO: send email
-            pass
+            result = RateLimiter.hit(
+                scope="req_auth_code_passwd_recovery",
+                key=email,
+                expiration=AuthService.BLOCK_TIME,
+            )
+
+            if result.allowed is False:
+                raise TooManyAttempts(seconds=result.remaining)
+
+            recover_password = RecoverPassword.objects.get_or_create(user=user)[0]
+            recover_password.generate_code()
+
+            MailService.send_password_recovery_code(email, code=recover_password.code)
 
     @staticmethod
     def verify_auth_code(email: str, code: str, auth_code: AuthCodeEnum):
