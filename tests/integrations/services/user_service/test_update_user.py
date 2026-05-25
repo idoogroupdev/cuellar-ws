6aac72a3300f05be99900f6ba55ee424a18a5102
raulodev <raulodev@gmail.com>
2026-05-25 16:50:32 -0400

    feat: add test for updating user with nullable last name field

--- tests/integrations/services/user_service/test_update_user.py
+++ tests/integrations/services/user_service/test_update_user.py
@@ -90,3 +90,14 @@ def test_update_user_password_and_extra_fields(setup_system_roles):
 
     assert updated_user.check_password("N3wStr0ngPass!123")
     assert updated_user.first_name == "New"
+
+
+@pytest.mark.django_db
+def test_update_user_field_not_nullable(setup_system_roles):
+    user = UserService.create_user_with_role(
+        email=fake.email(),
+        password="Str0ngPass!123",
+        role_name=DefaultSystemRole.CLIENT,
+    )
+
+    UserService.update_user(user, last_name=None)
