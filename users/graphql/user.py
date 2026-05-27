27d796852453a3374d7ed8e566724a200e68c61a
raulodev <raulodev@gmail.com>
2026-05-27 16:51:25 -0400

    feat: add CreateStaffUser mutation with input validation and permissions

--- users/graphql/user.py
+++ users/graphql/user.py
@@ -6,7 +6,7 @@ from graphene_file_upload.scalars import Upload
 from roles.graphql.roles import PermissionNode
 from users.models import User
 from users.services.user_service import UserService
-from utils.decorators import login_required
+from utils.decorators import login_required, permission_required, staff_member_required
 from utils.exceptions import ValidationGraphQLError
 
 
@@ -54,6 +54,41 @@ class UpdateMe(graphene.Mutation):
         return UpdateMe(user=user)
 
 
+class CreateStaffUserInput(graphene.InputObjectType):
+    email = graphene.String(required=True)
+    password = graphene.String(required=True)
+    role_name = graphene.String(required=True)
+    first_name = graphene.String(required=True)
+    last_name = graphene.String(required=False)
+    phone = graphene.String(required=False)
+
+
+class CreateStaffUser(graphene.Mutation):
+    user = graphene.Field(UserNode)
+
+    class Arguments:
+        input = CreateStaffUserInput(required=True)
+
+    @staff_member_required
+    @permission_required(User, ["add"])
+    def mutate(self, info, input: CreateStaffUserInput):
+        try:
+            user = UserService.create_user_with_role(
+                email=input.email.lower(),
+                password=input.password,
+                role_name=input.role_name,
+                first_name=input.first_name,
+                last_name=input.last_name,
+                phone=input.phone,
+                is_staff=True,
+                is_verified=True,
+            )
+        except ValidationError as exc:
+            raise ValidationGraphQLError(fields=exc.message_dict)
+
+        return CreateStaffUser(user=user)
+
+
 class Query(graphene.ObjectType):
     me = graphene.Field(UserNode)
 
@@ -64,6 +99,7 @@ class Query(graphene.ObjectType):
 
 class Mutation(graphene.ObjectType):
     update_me = UpdateMe.Field()
+    create_staff_user = CreateStaffUser.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
