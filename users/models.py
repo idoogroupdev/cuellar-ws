53308006432b6c0862aa416acbee4dd802018f70
raulodev <raulodev@gmail.com>
2026-06-22 10:06:20 -0400

    feat: add CashbackHistory model with validation and constraints

--- users/models.py
+++ users/models.py
@@ -136,3 +136,49 @@ class RecoverPassword(models.Model):
 
     def is_valid_code(self, code: str):
         return self.code == code
+
+
+class CashbackHistory(models.Model):
+    class TypeChoices(models.TextChoices):
+        EARNED = "EARNED", _("Earned")
+        SPENT = "SPENT", _("Spent")
+        RESERVED = "RESERVED", _("Reserved")
+        RELEASED = "RELEASED", _("Released")
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="cashback_history"
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=8, choices=TypeChoices.choices)
+    balance_after = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
+    released_with = models.OneToOneField(
+        "self", on_delete=models.PROTECT, null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(amount__gt=0),
+                name="check_cashback_history_amount_gt_zero",
+            ),
+            models.CheckConstraint(
+                condition=Q(released_with__isnull=True) | Q(type="RESERVED"),
+                name="released_with_only_for_reserved_type",
+            ),
+        ]
+
+    def clean(self):
+        if self.released_with:
+            if self.type != self.TypeChoices.RESERVED:
+                raise ValidationError(
+                    {"released_with": _("Cannot be used with a non-reserved type")}
+                )
+
+            if self.released_with.type != self.TypeChoices.RELEASED:
+                raise ValidationError(
+                    {"released_with": _("Cannot be used with a non-released type")}
+                )
