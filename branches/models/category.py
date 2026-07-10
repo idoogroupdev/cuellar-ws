03d12c40b92fdfa6278dff2ca8cd9096054751c1
raulodev <raulodev@gmail.com>
2026-07-10 09:01:37 -0400

    feat: add CategoryQuerySet

--- branches/models/category.py
+++ branches/models/category.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 
+class CategoryQuerySet(models.QuerySet):
+    def active(self):
+        return self.filter(is_active=True)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     is_active = models.BooleanField(default=True)
     sort_order = models.IntegerField(default=0)
+
+    objects = CategoryQuerySet.as_manager()
 
     class Meta:
         ordering = ["sort_order"]
